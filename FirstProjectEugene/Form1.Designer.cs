4c28198dc2f72c00452fe65b4219c40e31c62aed
PashinEugeneLAB <eugenepashin@mail.ru>
2023-09-26 23:28:43 +0500

    !!!!

--- FirstProjectEugene/Form1.Designer.cs
+++ FirstProjectEugene/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.sbStart = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
             this.SuspendLayout();
             // 
             // sbStart
@@ -39,11 +40,20 @@
             this.sbStart.TabIndex = 0;
             this.sbStart.Text = "Старт";
             // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(214, 12);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(196, 23);
+            this.simpleButton1.TabIndex = 1;
+            this.simpleButton1.Text = "Старт";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(604, 395);
+            this.Controls.Add(this.simpleButton1);
             this.Controls.Add(this.sbStart);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -54,6 +64,7 @@
         #endregion
 
         private DevExpress.XtraEditors.SimpleButton sbStart;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
     }
 }
 
